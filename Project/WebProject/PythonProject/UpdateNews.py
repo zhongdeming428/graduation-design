aedbfb71d6fe72a5351413af7897068ce136cce5
zhongdeming428 <570160841@qq.com>
2018-04-25 10:45:20 +0800

    写完了新闻获取的爬虫，将数据存储到了数据库。

--- Project/WebProject/PythonProject/UpdateNews.py
+++ Project/WebProject/PythonProject/UpdateNews.py
@@ -4,9 +4,29 @@
 import urllib.request as request
 from pyquery import PyQuery as pq
 from pymongo import MongoClient
+import time as T
 
-url = r'http://bond.hexun.com/news/index.html'
-content = request.urlopen(url).read().decode('gb2312')
+url = r'http://bond.eastmoney.com/news/czqxw.html'
+content = request.urlopen(url).read().decode('utf-8')
+count = 0
+# print(content)
 query = pq(content)
-div = query('#temp01')
-print(lis)
+divs = query('.text')
+data = []
+for div in divs:
+    doc = {}
+    doc['Title'] = div[0][0].text.replace('\r\n', '').replace(' ', '')
+    doc['Url'] = div[0][0].attrib['href']
+    data.append(doc)
+    count = count + 1
+times = query('p.time')
+for i, time in enumerate(times):
+    data[i]['Time'] = time.text.replace('\r\n', '').replace(' ', '')
+    data[i]['UpdateTime'] = T.time()
+# print(data)
+# 插入数据库。
+collection = MongoClient('localhost', 27017).BondsData.News
+for d in data:
+    if collection.find_one(d) == None:
+        collection.insert_one(d)
+print('更新' + str(count) + '条债券新闻。')
